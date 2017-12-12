bfa024282ae1d9de89ab5af34ce74c45a510195e
xtoblizi <ogbosukachris@gmail.com>
2017-12-12 18:32:59 +0100

    contact form effected added image class for img concerns.

---
+++ TFMCooperativeSociety/TFMCooperativeSociety/Models/ImageFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TFMCooperativeSociety.Models
+{
+    public class ImageFile
+    {
+        public int ImageFileId { get; set; }
+
+        public string FileName { get; set; }
+
+        public string FileExt { get; set; }
+
+        public string FilePath { get; set; }
+        public HttpPostedFileBase Image { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateCreate { get; set; }=DateTime.Now;
+    }
+}
